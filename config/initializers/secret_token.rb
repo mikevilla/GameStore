4703668b3758f2d636d02646c0e19d21bd6ba211
Mike Villa <mike@gamekultur.com>
2010-11-04 21:35:37 -0700

    Upgrade GameStore to Rails3

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GameStoreRails3::Application.config.secret_token = '73f5917a2dd433d5834a03ce45213581f345956963dbe847a20921ae48d987407a03e43994f3293f6082c2b1d57f84a107a9ae70fbcf5eb5d35e55cefd38bb1a'
